e05b71b29eabb43b20cef9e8e5975c9afd339bf9
Tuan Le <tuan.le@altres.com>
2019-05-21 11:48:30 -1000

    First commit

---
+++ product-tracker/src/main/java/dev/tuanle/producttracker/ProductTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.tuanle.producttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductTrackerApplication.class, args);
+	}
+
+}
---
+++ product-tracker/src/test/java/dev/tuanle/producttracker/ProductTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.tuanle.producttracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
